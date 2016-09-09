9c2570c6bfb3821368fcc83d2babbb1ebc05cd56
Andrey Inyutsin <inutcin@yandex.ru>
2016-09-09 16:54:56 +0400

    Загрузка складов

--- .integration/1c_catalog.ajax.php
+++ .integration/1c_catalog.ajax.php
@@ -27,14 +27,93 @@
     );
     
     $uploadDir = $_SERVER["DOCUMENT_ROOT"]."/upload/1c_catalog/";
+    // Получаем имя файла каталога
     $dd = opendir($uploadDir);
     $importFilename = '';
     while($filename = readdir($dd))
         if(preg_match("#^import.*\.xml$#",$filename))
             {$importFilename = $filename;break;}
+    closedir($dd);
     
-    
-    echo "|$importFilename|";
+    // Получаем имя файла товарных предложений
+    $dd = opendir($uploadDir);
+    $offersFilename = '';
+    while($filename = readdir($dd))
+        if(preg_match("#^offers.*\.xml$#",$filename))
+            {$offersFilename = $filename;break;}
+    closedir($dd);
+
+
+    if($offersFilename){
+        $xmlOffers = file_get_contents($uploadDir.$offersFilename);
+        $obOffers = simplexml_load_string(
+            $xmlOffers, "SimpleXMLElement" 
+        );
+        
+        $arOffers = json_decode(json_encode((array)$obOffers), TRUE);        
+        
+        $arStorages = $arOffers["ПакетПредложений"]["Склады"]["Склад"];
+        $arOffers = $arOffers["ПакетПредложений"]["Предложения"]["Предложение"];
+        
+        $arStoragesIndex = array();
+        foreach($arStorages as $arStorage){
+
+            $arStoragesIndex[$arStorage["Ид"]] = 0;
+
+            $arFields = array();
+            $arFields["TITLE"] = $arStorage["НаименованиеПолное"];
+            $arFields["ACTIVE"] = 'Y';
+            $arFields["ADDRESS"] = '';
+            $arFields["DESCRIPTION"] = '';
+            $arFields["GPS_N"] = '';
+            $arFields["GPS_S"] = '';
+            $arFields["IMAGE_ID"] = '';
+            $arFields["PHONE"] = '';
+            $arFields["SCHEDULE"] = '';
+            $arFields["XML_ID"] = $arStorage["Ид"];
+            
+            $arFields["USER_ID"] = '';
+            $arFields["EMAIL"] = '';
+            $arFields["ISSUING_CENTER"] = '';
+            $arFields["SHIPPING_CENTER"] = '';
+            $arFields["SITE_ID"] = 's1';
+            
+
+            if(
+                isset($arStorage["КакПроехать"]) 
+                && $arStorage["КакПроехать"]
+                && !is_array($arStorage["КакПроехать"])
+            )$arFields["ADDRESS"] .= $arStorage["КакПроехать"];
+                
+            if(
+                isset($arStorage["ДополнительнаяИнформация"]) 
+                && $arStorage["ДополнительнаяИнформация"]
+                && !is_array($arStorage["ДополнительнаяИнформация"])
+            )$arFields["DESCRIPTION"] .= $arStorage["ДополнительнаяИнформация"];
+
+            if(
+                isset($arStorage["ГрафикРаботы"]) 
+                && $arStorage["ГрафикРаботы"]
+                && !is_array($arStorage["ГрафикРаботы"])
+            )$arFields["SCHEDULE"] .= $arStorage["ГрафикРаботы"];
+
+            CModule::IncludeModule("catalog");
+            $res = CCatalogStore::GetList(array(),array(
+                "XML_ID"=>$arStorage["Ид"]
+            ),false,array("nTopCount"=>1));
+
+            if(!$existsStorage = $res->GetNext()){
+                $arStoragesIndex[$arStorage["Ид"]] = CCatalogStore::Add($arFields);
+            }
+            else{
+                $arStoragesIndex[$arStorage["Ид"]] = $existsStorage["ID"];
+                CCatalogStore::Update($existsStorage["ID"], $arFields);
+            }
+        }
+        echo "<pre>";
+        print_r($arStoragesIndex);
+        die;
+    }
 
     if($importFilename){
         
@@ -42,12 +121,12 @@
         $obImport = simplexml_load_string(
             $xmlImport, "SimpleXMLElement" 
         );
+        
         $arImport = json_decode(json_encode((array)$obImport), TRUE);        
+
         $arGroups = $arImport["Классификатор"]["Группы"];
         $arManufacturers = $arImport["Классификатор"]["Производители"]["Производитель"];
         $arProducts = $arImport["Каталог"]["Товары"]["Товар"];
-        
-        
         
         echo "<pre>";
         print_r($arManufacturers);
