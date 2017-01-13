3b512fb931ce3a47ed844260949e2579dc292c06
Andrey Inyutsin <inutcin@yandex.ru>
2017-01-13 14:27:48 +0400

    clear

--- bitrix/templates/.default/components/bitrix/catalog.store.amount/.default/result_modifier.php
+++
@@ -1,6 +0,0 @@
-<?
-foreach($arResult["STORES"] as $store_key=>$storeProperties){
-    $res = CCatalogStore::GetList(array(),array("ID"=>$storeProperties["ID"]));
-    $arResult["STORES"][$store_key]["DETAIL"] = $res->GetNExt();
-}
-
