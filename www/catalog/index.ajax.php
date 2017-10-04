6cf9d5c5218bb22e727619f159bf71f0d72e9292
Andrey Inyutsin <andrey.inutcin@altarix.ru>
2017-10-04 08:22:41 +0300

    Упс. Имитацию тупняка забыл

--- www/catalog/index.ajax.php
+++ www/catalog/index.ajax.php
@@ -4,7 +4,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/local/libs/customcache.lib.php");
 // Запись в ручной кэш (в обход битрикса)
 //customCache();
 //customCacheClear();
-sleep(1);
+//sleep(1);
 
 define("NO_KEEP_STATISTIC", true); // Не собираем стату по действиям AJAX
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
