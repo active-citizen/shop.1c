db86739c6c9236908e1bfd64cde58e6a918c9da7
Andrey Inyutsin <andrey.inutcin@altarix.ru>
2017-10-17 07:24:14 +0300

    Стирка-уборка

---
+++ www/api/sess.php
@@ -0,0 +1,31 @@
+<?php
+
+    require("include/common.php");
+
+    $arSessions = [];
+    for($i = 0;$i<100;$i++){;
+        $nN = sprintf("%02d",$i);
+        $GLOBALS["DB"]->search(["a"=>"sessions_".$nN],[],["user_id"=>0]);
+        $arSessions = array_merge($arSessions, $GLOBALS["DB"]->rows);
+        foreach($GLOBALS["DB"]->rows as $arRow){
+            $GLOBALS["DB"]->delete("sessions_".$nN,["user_id"=>0],"",0);
+        }
+        echo "<pre>$nN";
+        print_r($GLOBALS["DB"]->rows);
+        echo "</pre>";
+    }
+    $GLOBALS["DB"]->delete("transacts_detail_00",[],
+        "transaction_id IN (SELECT `a`.`id` FROM `transacts_brief_00` as `a`
+        WHERE `a`.`user_id`=0)"
+    ,0);
+    $GLOBALS["DB"]->delete("transacts_brief_00",["user_id"=>0], "" ,0);
+
+    $GLOBALS["DB"]->sql_query("OPTIMIZE TABLE `transacts_brief_00`");
+    $GLOBALS["DB"]->sql_query("OPTIMIZE TABLE `transacts_detail_00`");
+?>
+<script>
+setTimeout(function(){document.location.href='/api/sess.php';},30000);
+</script>
+
+
+
