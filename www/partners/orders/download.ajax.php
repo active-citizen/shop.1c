37f4033d3148174525d1beef982bf8d01e6ce758
Andrey Inyutsin <andrey.inutcin@altarix.ru>
2017-10-19 10:49:34 +0300

    fix

--- www/partners/orders/download.ajax.php
+++ www/partners/orders/download.ajax.php
@@ -426,7 +426,7 @@ if(isset($_REQUEST["continue"])){
             .";".'"'.str_replace("u","8",$arOrder["USER_LOGIN"]).'"' 
             .";".'" "'//'"Тип товара"'  
             .";".'"'.
-                htmlentities($arOrder["PRODUCT_NAME"],ENT_QUOTES|ENT_HTML401)
+                html_entity_decode($arOrder["PRODUCT_NAME"],ENT_QUOTES|ENT_HTML401)
             .'"'   
             .";".'""'//'"Модель"'  
             .";".'"'.($arOrder["STORE_NAME"]).'"'
