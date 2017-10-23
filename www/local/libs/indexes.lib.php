f7689ddf3e37d017d2ccc73c825b74a69c51a63c
Andrey Inyutsin <andrey.inutcin@altarix.ru>
2017-10-23 14:33:39 +0300

    AG-708

--- www/local/libs/indexes.lib.php
+++ www/local/libs/indexes.lib.php
@@ -2,16 +2,57 @@
     /**
         Синхронизация пользователя в общей таблице и индексной
     */
-    function syncUsers($nUserId){
+    function syncUser($nUserId){
         global $DB;
         $sQuery = "
             SELECT
                 `ID`, `LOGIN`, `NAME`, `LAST_NAME`, `EMAIL`
             FROM
-                `b_users` as `user`
+                `b_user` as `user`
             WHERE
                 `user`.`ID`=".$DB->ForSql($nUserId)."
+            LIMIT 
+                1
         ";
-        echo $sQuery;
+        $arUser = $DB->Query($sQuery)->Fetch();
 
+        $sQuery = "
+            SELECT
+                `ID`, `LOGIN`, `NAME`, `LAST_NAME`, `EMAIL`
+            FROM
+                `index_user` as `user`
+            WHERE
+                `user`.`ID`=".$DB->ForSql($nUserId)."
+            LIMIT 
+                1
+        ";
+        $arExistsUser = $DB->Query($sQuery)->Fetch();
+
+        if($arExistsUser){
+            $sQuery = "
+                UPDATE
+                    `index_user`
+                SET
+                    `LOGIN`='".$arUser["LOGIN"]."',
+                    `NAME`='".$arUser["NAME"]."',
+                    `LAST_NAME`='".$arUser["LAST_NAME"]."',
+                    `EMAIL`='".$arUser["EMAIL"]."'
+                WHERE
+                    `ID`='".$arUser["ID"]."'
+                LIMIT 1
+            ";
+        }
+        else{
+            $sQuery = "
+                INSERT INTO `index_user`(`ID`,`LOGIN`,`NAME`,`LAST_NAME`,`EMAIL`)
+                VALUES(
+                    '".$arUser["ID"]."',
+                    '".$arUser["LOGIN"]."',
+                    '".$arUser["NAME"]."',
+                    '".$arUser["LAST_NAME"]."',
+                    '".$arUser["EMAIL"]."'
+                )
+            ";
+        }
+        $DB->Query($sQuery);
     }
