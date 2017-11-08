6102bd6bfb467bd8fff22aca2c1f294f45497f55
inutcin <inutcin@yandex.ru>
2017-11-08 14:20:07 +0300

    AG-867

--- www/.integration/includes/import.inc.php
+++ www/.integration/includes/import.inc.php
@@ -197,6 +197,7 @@
             //"ACTIVE"    => preg_match("#Транспорт#",$arGroup["Наименование"])?"N":"Y",
             "SORT"      => $arGroup["Сортировка"],
             "IBLOCK_ID" => $CATALOG_IBLOCK_ID,
+            "ACTIVE"    => $arGroup["Включен"]=='Да'?"Y":"N",
             "XML_ID"    => $arGroup["Ид"],
         );
         // Транслитируем название к коду раздела
