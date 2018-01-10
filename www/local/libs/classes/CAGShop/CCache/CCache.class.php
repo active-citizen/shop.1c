0ee46e1b08a2fa789d5c0fc12a9f888248e64295
inutcin <inutcin@yandex.ru>
2018-01-10 10:34:05 +0300

    AG-1493

--- www/local/libs/classes/CAGShop/CCache/CCache.class.php
+++ www/local/libs/classes/CAGShop/CCache/CCache.class.php
@@ -1,23 +1,57 @@
 <?php
     namespace Cache;
-   
+
+    /**
+        Класс для кеширования
+    */
     class CCache {
         
         var $sKey = '';
         var $sGroup = '';
         var $nExpires = 300;
         var $sFullKey = '';
-        var $sBasePath = '';
+
+        private $objMemcached = null;
+
+        private $sBasePath = '';    //!< Киаталог для хранения кэша в файловой
+        // системе
         
+        /**
+            @param $sGroup - группа ключей кеширования
+            @param $sKey - ключ кеширования
+            @param $nExpires - время жизни кеша (секунд)
+        */
         function __construct($sGroup, $sKey, $nExpires=''){
             $this->sGroup = $sGroup;
             $this->sKey = $sKey;
             $this->nExpires = $nExpires;
             $this->sFullKey = "$sGroup:$sKey";
             $this->sBasePath = $_SERVER["DOCUMENT_ROOT"]."/../tmp/CCache";
+
+            // Проверяем доступность соединения с memcached
+            $arConfig = $this->__getBitrixConfig();
+            if(!isset($arConfig["cache"]["value"]["memcache"]["host"]))
+                return false;
+            if(!isset($arConfig["cache"]["value"]["memcache"]["port"]))
+                return false;
+            $nPort = $arConfig["cache"]["value"]["memcache"]["port"];
+            $sHost = preg_replace(
+                "#^unix://(.*)$#",
+                "$1",
+                $arConfig["cache"]["value"]["memcache"]["host"]
+            );
+
+            if(!class_exists("Memcached"))return false;
+            $this->objMemcached = new \Memcached();
+            if(!$this->objMemcached->addServer($sHost, $nPort))return false;
+            $this->bMemcached = true;
+            return true;
         }
         
         function get(){
+            if($this->objMemcached && $objData =  $this->__memcachedGet())
+                return $objData;
+
             if(!$this->getKeyFilename())return false;
             $stat = stat($this->getKeyFilename());
             if($stat['mtime']+$this->nExpires < time()){
@@ -28,6 +62,11 @@
         }
         
         function set($sValue){
+
+            if(
+                $this->objMemcached && $objData = $this->__memcachedSet($sValue)
+            )return true;
+            
             $sData = serialize($sValue);
             if(!is_dir($this->getKeyPath()))$this->createPath();
             $fd = fopen($this->getKeyFilename(),"w");
@@ -47,6 +86,34 @@
         private function getKeyFilename(){
             return $this->getKeyPath()."/".$this->sKey.".cache";
         }
+
+        /**
+            Получаем значение ключа из memcached
+        */
+        private function __memcachedGet(){
+            return unserialize($this->objMemcached->get($this->sFullKey));
+        }
+
+        /**
+            Получаем значение ключа из memcached
+        */
+        private function __memcachedSet($objValue){
+            return $this->objMemcached->set(
+                $this->sFullKey,
+                serialize($objValue),
+                $this->nExpires
+            );
+        }
+
+        private function __getBitrixConfig(){
+            $sConfigFilename =
+               $_SERVER["DOCUMENT_ROOT"]."/bitrix/.settings_extra.php";
+            if(!file_exists($sConfigFilename)){
+                $this->bMemcached = false;
+                return false;
+            }
+            return include($sConfigFilename);
+         }
         
     }
    
