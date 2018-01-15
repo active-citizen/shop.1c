ba2b808f32851321f73f345ec593634e41dae3ba
inutcin <inutcin@yandex.ru>
2018-01-15 16:26:04 +0300

    AG-1560

--- www/catalog/index.mobile.sections.php
+++ www/catalog/index.mobile.sections.php
@@ -9,12 +9,16 @@
     require_once($_SERVER["DOCUMENT_ROOT"]
         ."/local/libs/classes/CAGShop/CCatalog/CCatalogProductTag.class.php"
     );
+    require_once($_SERVER["DOCUMENT_ROOT"]
+        ."/local/libs/classes/CAGShop/CCatalog/CCatalogStore.class.php"
+    );
     use AGShop\Catalog as Catalog;
 
+    $arResult = [];
+
+    // Фильтр по интересам
     $objTag = new \Catalog\CCatalogProductTag(INTEREST_PROPERTY_ID);
     $arTags = $objTag->getAllTags();
-    
-    $arResult = [];
     foreach($arTags as $arTag){
         $sKey = CUtil::translit($arTag["NAME"],"ru",[
             "change_case"   =>  false,
@@ -27,7 +31,37 @@
             "url"=>"/catalog/?productInterest$sKey=".$arTag["ID"]
         ];
     }
+
+
+    // Фильтр по складам
+    $objStore = new \Catalog\CCatalogStore;
+    $arStores = $objStore->getForSite();
+    foreach($arStores['stores'] as $arStore){
+        $sKey = CUtil::translit($arStore["TITLE"],"ru",[
+            "change_case"   =>  false,
+            "replace_space" =>  "",
+            "replace_other" =>  ""
+        ]);
+        
+        $arResult[] = [
+            "category"=>$arStore["TITLE"],
+            "url"=>"/catalog/?productDelivery$sKey=".$arStore["ID"]
+        ];
+    }
+
+    // Фильтр по флагам
+    $arResult[] = [
+        "category"=>"Хит", "url"=>"/catalog/?productHitCheckbox=1"
+    ];
+    $arResult[] = [
+        "category"=>"Новинки", "url"=>"/catalog/?productNewCheckbox=2"
+    ];
+    $arResult[] = [
+        "category"=>"Акции", "url"=>"/catalog/?productSaleCheckbox=3"
+    ];
     
+
+
     echo json_encode($arResult);
 
 
