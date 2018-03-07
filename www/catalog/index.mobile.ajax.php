a71d74119d239229efcd3187191a893d1948af9a
inutcin <inutcin@yandex.ru>
2018-03-07 17:59:04 +0300

    fix

--- www/catalog/index.mobile.ajax.php
+++ www/catalog/index.mobile.ajax.php
@@ -2,7 +2,7 @@
     // Включаем безбитриксовое кеширование
     require($_SERVER["DOCUMENT_ROOT"]."/local/libs/customcache.lib.php");
     // Запись в ручной кэш (в обход битрикса)
-    //customCache();
+    customCache();
     require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
     require("mobile.filter.params.php");
 
