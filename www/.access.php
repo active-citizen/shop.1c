be24737b8869262282a70abdb58fc5dc1de3854a
Alexey Gevondyan <gevondyanab@altarix.ru>
2018-04-24 16:04:29 +0300

    ignore bitrix files

--- www/.access.php
+++ www/.access.php
@@ -1,15 +1,3 @@
 <?
-$PERM["personal"]["8"]="W";
-$PERM["catalog"]["8"]="W";
-$PERM["news"]["8"]="W";
-$PERM["about"]["8"]="W";
-$PERM["index.php"]["8"]="W";
 $PERM["/"]["*"]="R";
-$PERM["partners"]["*"] = "D";
-$PERM["partners"]["9"] = "W";
-$PERM["partners"]["10"] = "W";
-$PERM["partners"]["7"] = "W";
-
-
-
-
+?>
